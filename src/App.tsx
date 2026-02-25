83c902e14cef23eb1bcc13bff842974342cc82b9
parbati <pkit24a.pbt@ismt.edu.np>
2026-02-25 18:25:44 +0545

    fix routing for vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
-import { BrowserRouter, Route,Routes, } from 'react-router-dom';
+// import { BrowserRouter, Route,Routes, } from 'react-router-dom';
+// import './App.css'
+// import { Home } from './Home/page';
+// import { About } from './about/page';
+// import { Contact } from './contact/page';
+// import { Blog } from './blog/page';
+// //import { Route} from 'react-router-dom';
+
+// function App() {
+
+//  return(
+//   <BrowserRouter>
+//   <Routes>
+//     <Route path="/" element={<Home />} />
+//      <Route path="/about" element={<About />} />
+//       <Route path="/contact " element={<Contact />} />
+//       <Route path="/blog" element={<Blog />} />    
+//         </Routes>
+//        <div>Hello World!</div>
+//       </BrowserRouter>
+//    );
+// }
+// export default App;
+ 
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import { Home } from './Home/page';
-import { About } from './about/page';
-import { Contact } from './contact/page';
-import { Blog } from './blog/page';
-//import { Route} from 'react-router-dom';
+import { Home } from './Home/page'
+import { About } from './about/page'
+import { Contact } from './contact/page'
+import { Blog } from './blog/page'
 
 function App() {
-
- return(
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />} />
-     <Route path="/about" element={<About />} />
-      <Route path="/contact " element={<Contact />} />
-      <Route path="/blog" element={<Blog />} />    
-        </Routes>
-       <div>Hello World!</div>
-      </BrowserRouter>
-   );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/blog" element={<Blog />} />
+    </Routes>
+  )
 }
-export default App;
+
+export default App
